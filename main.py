12964e3536512882fb70f0a5a46c9ad3b5047831
braemp4 <spiderbraeden@gmail.com>
2025-06-11 22:10:05 -0400

    added more methods to bird

--- main.py
+++ main.py
@@ -28,12 +28,34 @@ class bird(pygame.sprite.Sprite):
           super().__init__()
           self.position = position
           self.image = img
-          self.rect = self.image.get_rect(topleft=self.position)
-
+          self.rect = self.image.get_rect(topleft=self.position),
+          self.score = 0
+          self.alive = False
+     def spawn_bird(self):
+          pass #draw bird at start pos
+          
      def flap(self, dy):
-          self.rect.y -= dy
+          self.rect.y += dy
 
+     def apply_gravity(self, grav):
+          self.rect.y -= grav #goes at top of loop
+          
+     def get_score(self):
+          return self.score
 
+     def add_score(self):
+          self.score += 1
+          
+     def game_over(self):
+          self.alive = False
+
+     def new_game(self):
+          if not self.alive:
+               self.alive = True
+          #spawn_bird gets called here
+     
+GRAVITY= -10 #subtract this from height to get falling
+FLAP_POWER = -13 #add to get upwards motion
 
 running = True
 
@@ -41,6 +63,9 @@ while running:
     
     pygame.display.update()
 
+     #we get menu with high score and strart game (first as cli then as gui)
+
+     
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
